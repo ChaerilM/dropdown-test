198bd21d1a32117e644924cc5f89bbe05a0fb301
Chaeril <chaeril@live.com>
2024-07-28 06:26:07 +0700

    update storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+/** @type { import('@storybook/html').Preview } */
+import '../src/tailwind.css';
+
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
