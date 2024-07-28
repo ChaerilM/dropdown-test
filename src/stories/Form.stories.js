e3c72d626139d94c82d60fe6ea6fc0a97d8d2ee2
Chaeril <chaeril@live.com>
2024-07-28 07:11:15 +0700

    add multiple

--- src/stories/Form.stories.js
+++ src/stories/Form.stories.js
@@ -15,6 +15,7 @@ export default {
         onChange: { action: 'onChange' },
         outlined: { control: 'boolean' },
         withSearch: { control: 'boolean' },
+        multiple: { control: 'boolean' },
     },
     // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
     args: { onChange: fn() },
