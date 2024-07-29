7ec1b90504577e5101178877c8065e744040c77b
Chaeril <chaeril@live.com>
2024-07-29 18:13:00 +0700

    add app and test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Dropdown from './Dropdown';
+import './tailwind.css';
 
 function App() {
+  const options = [
+    { value: 'option1', label: 'Option 1' },
+    { value: 'option2', label: 'Option with icon' },
+    { value: 'option3', label: 'Long Long Option 3' },
+    { value: 'option4', label: 'Long Long Long Option 4' },
+    { value: 'option5', label: 'Long Long Long Long Option 5' },
+    { value: 'option6', label: 'Long Long Long Long Long Option 6' },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/*  */}
       </header>
+      <div className='container p-8'>
+        <div className="flex flex-row">
+          <div className="md:w-1/6 w-1/3">
+            <label>Label</label>
+          </div>
+          <div className="md:w-5/6 w-2/3">
+            <Dropdown options={options} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders label label', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/label/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders dropdown with correct options', () => {
+  render(<App />);
+
+  const dropdown = screen.getByRole('combobox');
+  expect(dropdown).toBeInTheDocument();
+
+  // Simulate a click event on the dropdown
+  fireEvent.click(dropdown);
+
+  const options = screen.getAllByRole('option');
+  expect(options).toHaveLength(6);
+
+  expect(options[0]).toHaveTextContent('Option 1');
+  expect(options[1]).toHaveTextContent('Option with icon');
+  expect(options[2]).toHaveTextContent('Long Long Option 3');
+  expect(options[3]).toHaveTextContent('Long Long Long Option 4');
+  expect(options[4]).toHaveTextContent('Long Long Long Long Option 5');
+  expect(options[5]).toHaveTextContent('Long Long Long Long Long Option 6');
+});
+
